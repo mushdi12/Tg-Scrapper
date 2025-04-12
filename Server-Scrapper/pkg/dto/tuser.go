cdd68c49ba7b088a00c2ea6cfbffa2bc772036db
braginrom2005@gmail.com <mushdi12@katana-76>
2025-04-12 13:35:47 +0300

    ..

--- Server-Scrapper/pkg/dto/tuser.go
+++ Server-Scrapper/pkg/dto/tuser.go
@@ -1,1 +1,11 @@
 package dto
+
+import "fmt"
+
+type Tuser struct {
+	Username string `json:"username"`
+}
+
+func Print(ms string) {
+	fmt.Println(ms)
+}
--- Tg-bot/cmd/main.go
+++ Tg-bot/cmd/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/mushdi12/Tg-LinkTracker/tree/main/Server-Scrapper/pkg/dto "
 	. "link-tracker/internal/bot"
 )
 
 const filePath = "configs/config.json" // or switch to $PATH
 
 func main() {
+	P
 	bot := NewTgBot(filePath)
 	bot.Start()
 }
